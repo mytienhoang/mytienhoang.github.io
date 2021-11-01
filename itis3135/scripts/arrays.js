966e19d9965ae6ee5c4ef003649d47d0b30814fe
mytienhoang <mhoang4@uncc.edu>
2021-11-01 00:05:01 -0400

    sadf
    gp

--- itis3135/scripts/arrays.js
+++ itis3135/scripts/arrays.js
@@ -1,12 +1,37 @@
-const peoples = [];
-const salaries = [];
+const peoples = ["Bob Sheryll", "Nate Walkins", "Jonathan Nguyen", "Harry Potter", "Carl Bourne","Matt Teal"];
+const salaries = [0, 0, 0, 0, 0, 0];
+
+
+var listOfPeoples = "";
+for (var i = 0; i < salaries.length; i++ ) {
+        var person = "<option> " + peoples[i] + "</option>";
+        listOfPeoples = listOfPeoples + person;
+}
+document.getElementById("pickOneEmployee") = listOfPeoples;
+
 
 
 
 function addSalary() {
-    peoples[peoples.length] = prompt("Please Enter the Full Name of the Employee");
-    salaries[salaries.length] = prompt("Please Enter " + people[people.length-1] + " Salary.");
-    alert("Thanks for the info!");
+    var chosenPerson = document.getElementById("pickOneEmployee").value;
+    var salary = document.getElementById("theSalary").value;
+    salary = parseInt(salary);
+
+    if (salary == NaN) {
+        salary = 0;
+    }
+
+    for (var i = 0; i < peoples.length; i++) {
+        if (peoples[i] == chosenPerson) {
+            salaries[i] = salary;
+            alert(chosenPerson + "'s Salary is now " + salary);
+        }
+    }
+    alert(chosenPerson + "'s Salary is now " + salary);
+
+    /*peoples[peoples.length] = prompt("Please Enter the Full Name of the Employee");
+    salaries[salaries.length] = prompt("Please Enter " + peoples[peoples.length-1] + "'s Salary.");
+    alert("Thanks for the info!"); */
 }
 
 function displayResults() {
@@ -18,11 +43,22 @@ function displayResults() {
     }
     var totalSalary = 0;
     for (var i =0; i < salaries.length; i++) {
-        totalSalary = totalSalary + salaries[0];
+        totalSalary = totalSalary + salaries[i];
     }
-    var averageSalary = totalSalary/salaries.length;
-    document.getElementById("results").innerHTML = "<h2> Results <h2> <p> Average Salary is "+ totalSalary + ". </p> <p> Highest Salary is " + highestSalary;
+    var averageSalary = totalSalary/(salaries.length);
+    document.getElementById("results").innerHTML = "<h2> Results <h2> <p> Average Salary is "+ averageSalary + ". </p> <p> Highest Salary is " + highestSalary + "! </p>";
 
+}
+
+function listOfEmployees() {
+    var tableStatement = "<table>  <thead>  <tr>  <th>Employee Name</th>  <th>Salary per Year</th>  </tr>  </thead>  <tbody>";
+    for (var i = 0; i < salaries.length; i++ ) {
+        var employeeStatement = "<tr> <td>" + peoples[i] + "</td> <td> " + salaries[i] + "</td> </tr>";
+        tableStatement = tableStatement + employeeStatement;
+    }
+    tableStatement = tableStatement + "</tbody> </table>";
+    document.getElementById("tableForResults").innerHTML = tableStatement;
+    
 }
 
 
